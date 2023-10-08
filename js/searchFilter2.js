b2eb0e027c950ddfd9ee85aca3379f0408918bc4
Kaison Romero <kaisonjames19@gmail.com>
2023-10-07 19:17:26 -1000

    search filter
    attempting to make my own version of a search filter.

---
+++ js/searchFilter2.js
@@ -0,0 +1,18 @@
+const searchInput = document.getElementById("search");
+const images = document.querySelectorAll("a");
+
+
+searchInput.addEventListener("keyup", (event) => {
+  constsearchTerm = searchInput.value.toLowerCase();
+  for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+
+    const imageCaption = image.getAttribute("data-caption");
+
+    if (imageCaption.toLowerCase().indexOf(searchTerm) === -1) {
+      image.style.display = "none";
+    } else {
+      image.style.display = "block";
+    }
+  }
+});
